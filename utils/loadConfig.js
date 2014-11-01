04ef4adf58dd7f52f81918b2deb30d4b3baf786c
Jacobo Tabernero <jacobo@xarope.eu>
2014-10-31 19:09:47 -0600

    add deepjson

--- utils/loadConfig.js
+++ utils/loadConfig.js
@@ -3,13 +3,14 @@
 var fs = require('fs'),
 	deepExtend = require('deep-extend'),
 	path = require('path'),
-	defConf = require('./default-config.json');
+	defConf = require('./default-config.json'),
+	deepjson = require('deepjson');
 
 module.exports = function (dir) {
 	var env = process.env.NODE_ENV || 'default',
 		configDir = path.resolve( dir, 'config'),
 		hwConfPath = path.resolve( dir, 'hw-conf.json'),
-		config = require( hwConfPath ),
+		config = deepjson( hwConfPath ),
 		envConfig;
 
 	config = deepExtend( defConf, config );
@@ -17,7 +18,7 @@ module.exports = function (dir) {
 
 	if (env !== 'default') {
 		if (fs.existsSync( path.resolve( configDir , env + '.json' ))) {
-			envConfig = require( path.resolve( configDir , env + '.json' ));
+			envConfig = deepjson( path.resolve( configDir , env + '.json' ));
 			config = deepExtend( config, envConfig );
 		}
 	}
