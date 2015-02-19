56791ead4799c75f1fa1412cb615ae2dd9f0c575
Jacobo Tabernero <jacobo@xarope.eu>
2015-02-19 14:20:17 -0600

    avoid npm to change .gitignore filename at boilerplate

--- utils/boilerplate.js
+++ utils/boilerplate.js
@@ -1,19 +1,21 @@
 'use strict';
-var path = require( 'path' ),
-	ncp = require( 'ncp' ),
-	mkdirp = require( 'mkdirp' ),
-	dir = path.resolve( '.' );
+var path = require('path'),
+	ncp = require('ncp'),
+	mkdirp = require('mkdirp'),
+	cp = require('cp'),
+	dir = path.resolve('.'),
+	bFolder = path.resolve( __dirname, './boilerplate' ),
+	files = ['app.js', 'hw-conf.json', 'README.md'];
 
-
-ncp( path.resolve( __dirname, './boilerplate' ), dir, {clobber: false},function (err) {
-	if (err) {
-		console.error( err );
-		process.exit( 1 );
-	}
-	mkdirp( dir + '/app/views');
-	mkdirp( dir + '/app/models');
-	mkdirp( dir + '/app/controllers');
-	mkdirp( dir + '/app/routes');
-	mkdirp( dir + '/app/public');
-	console.log('Basic boilerplate installed!');
+files.forEach( function (file) {
+	cp.sync(bFolder + '/' + file, dir + '/' + file);
 });
+cp.sync(bFolder + '/gitignore', dir + '/.gitignore');
+
+mkdirp( dir + '/app/views');
+mkdirp( dir + '/app/models');
+mkdirp( dir + '/app/controllers');
+mkdirp( dir + '/app/routes');
+mkdirp( dir + '/app/public');
+
+console.log('Basic boilerplate installed!');
